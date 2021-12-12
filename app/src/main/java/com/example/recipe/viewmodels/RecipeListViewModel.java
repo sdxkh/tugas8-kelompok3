01e66bd47129e3503d7fbe9031593f30418b138b
Echa <sdxkh0203@gmail.com>
2021-12-13 00:07:45 +0800

    tugas 8

---
+++ app/src/main/java/com/example/recipe/viewmodels/RecipeListViewModel.java
@@ -0,0 +1,87 @@
+package com.example.recipe.viewmodels;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.ViewModel;
+
+import com.codingwithmitch.foodrecipes.models.Recipe;
+import com.codingwithmitch.foodrecipes.repositories.RecipeRepository;
+
+import java.util.List;
+
+public class RecipeListViewModel extends ViewModel {
+
+    private RecipeRepository mRecipeRepository;
+    private boolean mIsViewingRecipes;
+    private boolean mIsPerformingQuery;
+
+    public RecipeListViewModel() {
+        mRecipeRepository = RecipeRepository.getInstance();
+        mIsPerformingQuery = false;
+    }
+
+    public LiveData<List<Recipe>> getRecipes(){
+        return mRecipeRepository.getRecipes();
+    }
+
+    public LiveData<Boolean> isQueryExhausted(){
+        return mRecipeRepository.isQueryExhausted();
+    }
+
+    public void searchRecipesApi(String query, int pageNumber){
+        mIsViewingRecipes = true;
+        mIsPerformingQuery = true;
+        mRecipeRepository.searchRecipesApi(query, pageNumber);
+    }
+
+    public void searchNextPage(){
+        if(!mIsPerformingQuery
+                && mIsViewingRecipes
+                && !isQueryExhausted().getValue()){
+            mRecipeRepository.searchNextPage();
+        }
+    }
+
+    public boolean isViewingRecipes(){
+        return mIsViewingRecipes;
+    }
+
+    public void setIsViewingRecipes(boolean isViewingRecipes){
+        mIsViewingRecipes = isViewingRecipes;
+    }
+
+    public void setIsPerformingQuery(Boolean isPerformingQuery){
+        mIsPerformingQuery = isPerformingQuery;
+    }
+
+    public boolean isPerformingQuery(){
+        return mIsPerformingQuery;
+    }
+
+    public boolean onBackPressed(){
+        if(mIsPerformingQuery){
+            // cancel the query
+            mRecipeRepository.cancelRequest();
+            mIsPerformingQuery = false;
+        }
+        if(mIsViewingRecipes){
+            mIsViewingRecipes = false;
+            return false;
+        }
+        return true;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
